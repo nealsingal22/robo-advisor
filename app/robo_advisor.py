ee1fa3905e8d1dc55cb91c485998a1d284095b22
nealsingal22 <59627699+nealsingal22@users.noreply.github.com>
2020-02-28 09:33:11 -0500

    Writing to a csv file with stock data

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -43,16 +43,18 @@ csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.cs
 csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
 
 with open(csv_file_path, "w") as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=["city","name"])
+    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader()
-    writer.writerow({
-        "timestamp", 
-        "open", 
-        "high",
-        "low" 
-        "close", 
-        "volume"
-    })
+    for date in dates:
+        daily_prices = tsd[date]
+        writer.writerow({
+            "timestamp": date,
+            "open": daily_prices["1. open"],
+            "high": daily_prices["2. high"],
+            "low": daily_prices["3. low"],
+            "close": daily_prices["4. close"],
+            "volume": daily_prices["5. volume"]
+        })
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
