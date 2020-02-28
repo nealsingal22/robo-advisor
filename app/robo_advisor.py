c4266a130c738e254115fa759c7e30167d3784bb
nealsingal22 <59627699+nealsingal22@users.noreply.github.com>
2020-02-28 09:02:44 -0500

    Get latest close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,8 +3,11 @@
 import requests
 import json
 
+#Adapted from Shopping Cart project
+def to_usd(my_price):
+    return "${0:,.2f}".format(my_price)
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo"
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 response = requests.get(request_url)
 print(response.status_code)
 print(response.request)
@@ -15,6 +18,7 @@ parsed_respose = json.loads(response.text)
 
 last_refreshed = parsed_respose["Meta Data"]["3. Last Refreshed"]
 
+latest_close = parsed_respose["Time Series (Daily)"]["2020-02-27"]["4. close"]
 
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
@@ -22,8 +26,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: " +last_refreshed)
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
